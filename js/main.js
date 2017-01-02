f9da0ab8f4274653a574ef0bd0d1b1cd1fc8f6a0
onurkuru <onurfredy@gmail.com>
2017-01-02 22:09:42 +0300

    versiyon 1

---
+++ js/main.js
@@ -0,0 +1,60 @@
+var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
+var completeSVG = '<svg width="35px" height="35px" viewBox="14 28 35 35" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <desc>Created with Sketch.</desc> <defs> <path class="fill" d="M30.6221357,51.1034891 C30.6387188,51.0889814 30.6549298,51.0738242 30.6707362,51.0580177 L39.226897,42.501857 C39.6070206,42.1217334 39.6084747,41.4832069 39.2155404,41.0902726 L38.5018405,40.3765726 C38.1056387,39.9803709 37.4769183,39.9785538 37.0902561,40.365216 L29.3804961,48.074976 L26.1758856,44.8703654 C25.787114,44.4815938 25.1520861,44.4836383 24.7591518,44.8765726 L24.0454519,45.5902726 C23.6492501,45.9864743 23.6497385,46.6175002 24.0392446,47.0070063 L28.6736684,51.64143 C29.0624399,52.0302016 29.6974678,52.0281571 30.0904021,51.6352228 L30.6221357,51.1034891 Z M31.5,63 C41.1649831,63 49,55.1649831 49,45.5 C49,35.8350169 41.1649831,28 31.5,28 C21.8350169,28 14,35.8350169 14,45.5 C14,55.1649831 21.8350169,63 31.5,63 Z" id="path-1"></path> <mask id="mask-2" maskContentUnits="userSpaceOnUse" maskUnits="objectBoundingBox" x="0" y="0" width="35" height="35" fill="white"> <use xlink:href="#path-1"></use> </mask> </defs> <use id="icon-checkmark-unchecked" stroke="#A6C0D6" mask="url(#mask-2)" stroke-width="2" fill="none" xlink:href="#path-1"></use></svg>';
+var completeSVG2 = '<svg width="35px" height="35px" viewBox="14 28 35 35" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <defs></defs> <path d="M30.6221357,51.1034891 C30.6387188,51.0889814 30.6549298,51.0738242 30.6707362,51.0580177 L39.226897,42.501857 C39.6070206,42.1217334 39.6084747,41.4832069 39.2155404,41.0902726 L38.5018405,40.3765726 C38.1056387,39.9803709 37.4769183,39.9785538 37.0902561,40.365216 L29.3804961,48.074976 L26.1758856,44.8703654 C25.787114,44.4815938 25.1520861,44.4836383 24.7591518,44.8765726 L24.0454519,45.5902726 C23.6492501,45.9864743 23.6497385,46.6175002 24.0392446,47.0070063 L28.6736684,51.64143 C29.0624399,52.0302016 29.6974678,52.0281571 30.0904021,51.6352228 L30.6221357,51.1034891 Z M31.5,63 C41.1649831,63 49,55.1649831 49,45.5 C49,35.8350169 41.1649831,28 31.5,28 C21.8350169,28 14,35.8350169 14,45.5 C14,55.1649831 21.8350169,63 31.5,63 Z" id="icon-checkmark-checked" stroke="none" fill="#A6C0D6" fill-rule="evenodd"></path> </svg>';
+
+
+document.getElementById('add').addEventListener('click', function() {
+  var value = document.getElementById('item').value;
+  var value = document.getElementById('item').value;
+  if (value) {
+    addItemTodo(value);
+  }
+});
+
+
+
+function addItemTodo(text) {
+
+var list = document.getElementById('notdone');
+
+var item = document.createElement('li');
+
+var buttons = document.createElement('div');
+buttons.classList.add('check');
+
+var complete = document.createElement('button');
+complete.classList.add('complete');
+complete.innerHTML = completeSVG;
+
+var content = document.createElement('div');
+content.classList.add('content');
+content.innerHTML = text;
+
+var duedate = document.createElement('div');
+duedate.classList.add('duedate');
+duedate.innerHTML = '';
+
+
+buttons.appendChild(complete);
+item.appendChild(buttons);
+item.appendChild(content);
+item.appendChild(duedate);
+list.insertBefore(item, list.childNodes[0]);
+
+closeNav();
+}
+
+function openNav() {
+    document.getElementById("NewPage").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("NewPage").style.width = "0%";
+}
+function valueChanged()
+{
+    if($('label').is(":checked"))
+        $(".datebox").show();
+    else
+        $(".datebox").hide();
+}
